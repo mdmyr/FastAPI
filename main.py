a7cbb22979a4f9a8faa869307a41dad69b150430
YuvaRaj Raghunapu <mdm.yuvaraj@gmail.com>
2021-05-17 15:52:29 -0400

    Fast and Fist api using python and Node

---
+++ main.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def respond():
+    return{"This is no goods."}
+
+@app.get("/about")
+def about():
+    return { "This is a about page."}
